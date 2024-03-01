92b1089f2698031b9abaecf93b4d5f076d2a7e8c
rubenpio <152532760+rubenpio@users.noreply.github.com>
2024-03-01 12:38:24 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,13 +16,47 @@ resource "azurerm_container_registry" "acr" {
   location                 = azurerm_resource_group.rg.location
   sku                      = "Basic"
 }
+# Creación de la red virtual
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
 
+# Creación de un subred
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+# Creación de una dirección IP pública
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.ip_pub
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"  # Puedes cambiar a "Static" si necesitas una IP pública estática
+}
+# Creación de una interfaz de red
+resource "azurerm_network_interface" "interface" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
 # Creación de máquina virtual (VM) con sistema operativo Linux
 resource "azurerm_linux_virtual_machine" "vm" {
   name                            = var.vm_name
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  size                            = "Basic_A1"
+  size                            = "Standard_DS1_v2"
   admin_username                  = var.admin_username
   disable_password_authentication = true
 
@@ -32,7 +66,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     public_key = file(var.ssh_public_key_path)
   }
 # Seleccionamos una interfaz de red creada anteriormente
-  network_interface_ids = ["/subscriptions/ea75ca59-207c-4e10-bc03-dc481f4cd005/resourceGroups/federico/providers/Microsoft.Network/networkInterfaces/Interface1"]
+  network_interface_ids = [azurerm_network_interface.interface.id]
 # Escogemos el tamaño y caracteristicas que va a tener el dico de la maquina
   os_disk {
     disk_size_gb         = 32
@@ -69,5 +103,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   tags = {
     Environment = "Production"
   }
-}
+
+
 
